c0f84e38d4fe34b6b847dd08225e00b4920d82ce
shtopane <vechesumstudent@gmail.com>
2024-08-07 17:53:18 +0300

    add update method; add jax cache config

--- econpizza/config/__init__.py
+++ econpizza/config/__init__.py
@@ -3,6 +3,7 @@ class EconPizzaConfig(dict):
     def __init__(self, *args, **kwargs):
         super(EconPizzaConfig, self).__init__(*args, **kwargs)
         self._enable_persistent_cache = None
+        self._enable_jax_persistent_cache = None
 
     @property
     def enable_persistent_cache(self):
@@ -11,13 +12,30 @@ class EconPizzaConfig(dict):
     @enable_persistent_cache.setter
     def enable_persistent_cache(self, value):
         self._enable_persistent_cache = value
-    
+
+    @property
+    def enable_jax_persistent_cache(self):
+        return self._enable_jax_persistent_cache
+
+    @enable_jax_persistent_cache.setter
+    def enable_jax_persistent_cache(self, value):
+        self._enable_jax_persistent_cache = value
+
+    def update(self, key, value):
+        if hasattr(self, key):
+            setattr(self, key, value)
+        else:
+            raise AttributeError(f"'EconPizzaConfig' object has no attribute '{key}'")
+
     def __repr__(self):
-      properties = {k.lstrip('_'): v for k, v in self.__dict__.items() if k.startswith('_')}
-      return f"{properties}"
+        properties = {
+            k.lstrip("_"): v for k, v in self.__dict__.items() if k.startswith("_")
+        }
+        return f"{properties}"
 
     def __str__(self):
         return self.__repr__()
+
 
 config = EconPizzaConfig()
 
